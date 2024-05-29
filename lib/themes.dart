ef6f55167886c40c42cd754093e21ad189aaed36
Kabelo Peele <kabelom.p@live.com>
2024-05-29 10:06:16 +0200

    added character listView, created a character_card widget

--- lib/themes.dart
+++ lib/themes.dart
@@ -46,4 +46,13 @@ ThemeData primaryTheme = ThemeData(
       letterSpacing: 2,
     ),
   ),
+
+  // card theme
+  cardTheme: CardTheme(
+    color: AppColors.secondaryColor.withOpacity(0.5),
+    surfaceTintColor: Colors.transparent,
+    shape: const RoundedRectangleBorder(),
+    shadowColor: Colors.transparent,
+    margin: const EdgeInsets.only(bottom: 16),
+  ),
 );
