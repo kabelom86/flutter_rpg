8ac500cfc92595c5edfaa6c78f4e6edd750bf766
Kabelo Peele <kabelom.p@live.com>
2024-05-29 16:39:29 +0200

    added more model files of skill, stats and vocation with respective classes

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,18 +1,34 @@
-class Character {
+import 'package:flutter_rpg/models/skill.dart';
+import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+
+class Character with Stats {
   // Constructor
   Character({
     required this.name,
     required this.slogan,
     required this.id,
+    required this.vocation,
   });
 
   // fields
+  final Set<Skill> skills = {};
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
   bool _isFav = false;
 
+  // getters
+  bool get isFav => _isFav;
+
+  // methods
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 }
